2fcebc5adf997c9af93161258544919acd1288d4
DynamoOfVolition <catbern456@gmail.com>
2021-08-08 12:28:29 -0400

    first commit

---
+++ termProject.py
@@ -0,0 +1,33 @@
+from cmu_112_graphics import *
+
+
+def appStarted(app):
+    pass
+
+def keyPressed(app, event):
+    pass
+
+def mousePressed(app, event):
+    pass
+
+def timerFired(app):
+    pass
+
+def clickInGrid(app, x, y):
+    pass
+
+def getCell(app, x, y):
+    pass
+
+def getCellBounds(app, row, col):
+    pass
+
+def drawGrid(app, canvas):
+    pass
+
+def redrawAll(app, canvas):
+    pass
+
+
+runApp(width=650, height=650)
+
